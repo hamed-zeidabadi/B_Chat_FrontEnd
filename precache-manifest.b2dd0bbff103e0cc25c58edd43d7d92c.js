0cbc7c404f78791159818f670cc4cad50d511fc8
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-06-20 20:32:16 +0430

    Updates

---
+++ precache-manifest.b2dd0bbff103e0cc25c58edd43d7d92c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4066561898ba81fce3f6636b0a5ca760",
+    "url": "/Bchat/index.html"
+  },
+  {
+    "revision": "d68f2a0e33994209d914",
+    "url": "/Bchat/static/css/main.edf6dd07.chunk.css"
+  },
+  {
+    "revision": "2ce626c9f55226a0e5aa",
+    "url": "/Bchat/static/js/2.3ff2c5e5.chunk.js"
+  },
+  {
+    "revision": "57eec414799b75b36bb910d1db195bb7",
+    "url": "/Bchat/static/js/2.3ff2c5e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68f2a0e33994209d914",
+    "url": "/Bchat/static/js/main.d19230af.chunk.js"
+  },
+  {
+    "revision": "7fc65a2c78a6b5edf063",
+    "url": "/Bchat/static/js/runtime-main.d2dbcca7.js"
+  },
+  {
+    "revision": "860ad172ae5c052dea861911dc17b92a",
+    "url": "/Bchat/static/media/IRANSans.860ad172.ttf"
+  },
+  {
+    "revision": "c75cdfec9b624c2af15088cc9d88ce53",
+    "url": "/Bchat/static/media/avatar1.c75cdfec.png"
+  },
+  {
+    "revision": "82ab1f010dc53e9a7118078ab353a47a",
+    "url": "/Bchat/static/media/avatar2.82ab1f01.png"
+  },
+  {
+    "revision": "82ab1f010dc53e9a7118078ab353a47a",
+    "url": "/Bchat/static/media/avatar3.82ab1f01.png"
+  },
+  {
+    "revision": "c91f937b1063ed4566175a83838069eb",
+    "url": "/Bchat/static/media/avatar4.c91f937b.png"
+  },
+  {
+    "revision": "c7771f76644fc3f73d75c5885c4bd7e4",
+    "url": "/Bchat/static/media/avatar5.c7771f76.png"
+  },
+  {
+    "revision": "de78abc4cab303c770f7131e3713ffe4",
+    "url": "/Bchat/static/media/avatar6.de78abc4.png"
+  },
+  {
+    "revision": "994adf5e5b35c9c40db28f78f77b2f57",
+    "url": "/Bchat/static/media/avatar7.994adf5e.png"
+  },
+  {
+    "revision": "d899af103bf0e91a11a24b974bfc583d",
+    "url": "/Bchat/static/media/avatar8.d899af10.png"
+  },
+  {
+    "revision": "7b92f7a4360d2a4ba98af7ba01151c7b",
+    "url": "/Bchat/static/media/gradient.7b92f7a4.jpg"
+  },
+  {
+    "revision": "4886dbd3430c1bb3d69caacdbddb156d",
+    "url": "/Bchat/static/media/logo.4886dbd3.png"
+  }
+]);
